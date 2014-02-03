1fa2653ac068557a80f7fbe04e7772d80ef1cd69
Robert Leahy <rleahy@rleahy.ca>
2014-02-03 00:45:58 -0800

    Error Addition
    Added the Wrap method to the Error class -- which deals with PHP's internal error reporting.
    
    The Wrap method takes an arbitrary callback, invokes it with an arbitrary number of arguments, and transforms any PHP errors/warnings which occur during its execution into exceptions.

--- civicinfobc/error.php
+++ civicinfobc/error.php
@@ -28,6 +28,59 @@
 			);
 		
 		}
+		
+		
+		/**
+		 *	Wraps a callback, transforming any PHP warnings
+		 *	or errors which occur during that callback into
+		 *	exceptions which will be thrown when the callback
+		 *	returns.
+		 *
+		 *	An arbitrary number of arguments may be passed in.
+		 *	Any arguments past the first will be passed through
+		 *	as arguments to the callback.
+		 *
+		 *	\param [in] $callback
+		 *		The callback to invoke.
+		 *
+		 *	\return
+		 *		Whatever \em callback returned, if anything.
+		 */
+		public static function Wrap ($callback) {
+		
+			$arr=func_get_args();
+			unset($arr[0]);
+			
+			$ex=null;
+			set_error_handler(function ($errno, $errstr) use (&$ex) {
+			
+				if (error_reporting()===0) return false;
+			
+				$ex=new \Exception($errstr,$errno);
+				
+				return true;
+			
+			});
+			
+			try {
+			
+				$retr=call_user_func_array($callback,$arr);
+			
+			} catch (\Exception $e) {
+			
+				restore_error_handler();
+				
+				throw $e;
+			
+			}
+			
+			restore_error_handler();
+			
+			if (!is_null($ex)) throw $ex;
+			
+			return $retr;
+		
+		}
 	
 	
 	}
