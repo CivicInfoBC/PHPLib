623a29848e59dded7335c75174afda3499797767
Robert Leahy <rleahy@rleahy.ca>
2014-02-13 19:44:31 -0800

    ArrayUtil::Find
    Added the Find static method to the ArrayUtil class.  This method takes an array, a value, and optionally a boolean value and a comparison functor, and returns either the key at which the value exists in the array, or null if the value was not found.  The boolean value may be used to tell the function to throw rather than returning null.

--- civicinfobc/arrayutil.php
+++ civicinfobc/arrayutil.php
@@ -32,6 +32,52 @@
 		
 		
 		/**
+		 *	Attempts to find some element in an array.
+		 *
+		 *	\param [in] $arr
+		 *		The array.
+		 *	\param [in] $what
+		 *		The element.
+		 *	\param [in] $throw
+		 *		If \em true and \em what cannot be
+		 *		found in \em arr, a NotFoundException
+		 *		will be thrown, otherwise \em null
+		 *		will be returned in this case.  Defaults
+		 *		to \em false.
+		 *	\param [in] $comparer
+		 *		An optional callable object which will
+		 *		be used to compare objects for equality.
+		 *		Defaults to \em null.  If \em null
+		 *		strict comparison will be used.
+		 *
+		 *	\return
+		 *		The key associated with \em what in
+		 *		\em arr if \em what is in \em arr,
+		 *		\em null otherwise.
+		 */
+		public static function Find ($arr, $what, $throw=false, $comparer=null) {
+		
+			$arr=self::Coalesce($arr);
+			
+			foreach ($arr as $key=>$value) {
+			
+				if (is_null($comparer)
+					?	($what===$value)
+					:	$comparer($what,$value)
+				) return $key;
+			
+			}
+			
+			if ($throw) throw new NotFoundException(
+				'Value could not be found in array'
+			);
+			
+			return null;
+		
+		}
+	
+	
+		/**
 		 *	Checks an array to see if it contains
 		 *	some element.
 		 *
