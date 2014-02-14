254aa825bd78eb0c5c131edfabb004cd102cf37e
Robert Leahy <rleahy@rleahy.ca>
2014-02-14 02:28:40 -0800

    EliMina\Item Documentation
    Added documentation to the EliMina\Item class.

--- civicinfobc/elimina/item.php
+++ civicinfobc/elimina/item.php
@@ -4,14 +4,37 @@
 	namespace CivicInfoBC\EliMina;
 	
 	
+	/**
+	 *	A single item.
+	 */
 	class Item {
 	
 	
+		/**
+		 *	The name of the item.
+		 */
 		public $name;
+		/**
+		 *	The weight of the item.
+		 */
 		public $weight;
+		/**
+		 *	The height of the item
+		 *	(for packing purposes).
+		 */
 		public $height;
 		
 		
+		/**
+		 *	Creates a new Item.
+		 *
+		 *	\param [in] $name
+		 *		The name.
+		 *	\param [in] $weight
+		 *		The weight.
+		 *	\param [in] $height
+		 *		The height.
+		 */
 		public function __construct ($name, \CivicInfoBC\Measure $weight, \CivicInfoBC\Measure $height) {
 		
 			$this->name=$name;
