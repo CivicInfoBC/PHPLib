81c9dd6d535971186c18663767fa34bc54b7496a
Robert Leahy <rleahy@rleahy.ca>
2014-02-16 01:47:51 -0800

    Formatting Utilities
    Added the Format class, which will contain utilities for formatting strings for output.
    
    Currently contains only the static Name method, which takes an input string and formats it according to rules acceptable for display of names (corrects the case to Initial Caps, removes spurious whitespace, transforms whitespace into single space characters, and attempts to correct names which are exceptions to case conventions (e.g. "McDonald")).
    
    The Name method has 10 unit tests.

---
+++ civicinfobc/format.php
@@ -0,0 +1,98 @@
+<?php
+
+
+	namespace CivicInfoBC;
+	
+	
+	/**
+	 *	Contains utilities for formatting strings.
+	 */
+	class Format {
+	
+	
+		/**
+		 *	Formats/normalizes a name.
+		 *
+		 *	Removes spurious whitespace.
+		 *
+		 *	Replaces all whitespace that isn't
+		 *	the space character with the space
+		 *	character.
+		 *
+		 *	Corrects capitalization.
+		 *
+		 *	\param [in] $name
+		 *		A name.
+		 *
+		 *	\return
+		 *		\em name after formatting/normalization.
+		 */
+		public static function Name ($name) {
+		
+			//	Perform a few initial clean up
+			//	steps:
+			//
+			//	1.	Lower case the name (proper
+			//		capitalization will be re-added
+			//	2.	Trim leading/trailing whitespace
+			//	3.	Replace all sequences of whitespace
+			//		with single spaces.
+			$name=String::ToLower(
+				Regex::Replace(
+					'/(?<=^|\\S)\\s+(?=\\S|$)/u',
+					' ',
+					String::Trim($name)
+				)
+			);
+			
+			//	Filter the name to correct casing
+			//	et cetera
+			foreach (array(
+				//	First letter of each word
+				//	is upper cased
+				'/\\b(\\w)/u' => function ($matches) {
+				
+					return String::ToUpper($matches[1]);
+					
+				},
+				//	Transform names like "O'donnel"
+				//	to "O'Donnel"
+				'/(?<=\\bO\')(\\w)/u' => function ($matches) {
+				
+					return String::ToUpper($matches[1]);
+				
+				},
+				//	Transform names like "Mcdonald"
+				//	and "Macwilliam" to "McDonald" and
+				//	"MacWilliam", respectively
+				'/\\b(Ma?c)(\\w)/u' => function ($matches) {
+				
+					return $matches[1].String::ToUpper($matches[2]);
+				
+				},
+				//	Transform "van", "der", etc. to lower
+				//	case
+				'/\\b(v[ao]n|der?)\\b/ui' => function ($matches) {
+				
+					return String::ToLower($matches[1]);
+				
+				}
+			) as $p=>$s) $name=($s instanceof \Closure) ? Regex::ReplaceCallback(
+				$p,
+				$s,
+				$name
+			) : Regex::Replace(
+				$p,
+				$s,
+				$name
+			);
+			
+			return $name;
+		
+		}
+	
+	
+	}
+
+
+?>
