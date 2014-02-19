a72c2ca12748e871218d3129b710b8ee56666208
Robert Leahy <rleahy@rleahy.ca>
2014-02-19 01:41:02 -0800

    InputObject
    Implemented the InputObject class, which wraps an array which -- in turn -- wraps HTTP input -- such as query string or POST variables.
    
    This object seamlessly performs string preprocessing on these values, returning null instead of the empty string, and trimming values before returning them.
    
    Like StrictObject, by default attempting to get a non-existent property -- or one which is null under the object's understanding thereof (i.e. the empty string) -- throws, which makes validation extremely straightforward.

---
+++ civicinfobc/inputobject.php
@@ -0,0 +1,71 @@
+<?php
+
+
+	namespace CivicInfoBC;
+	
+	
+	class InputObject implements \IteratorAggregate {
+	
+	
+		private $kv;
+		
+		
+		public function __construct ($obj) {
+		
+			$this->kv=new KeyValueWrapper($obj);
+		
+		}
+		
+		
+		private function get_impl ($key) {
+		
+			if (!isset($this->kv->$key)) return null;
+			
+			$retr=String::Trim($this->kv->$key);
+			
+			return ($retr==='') ? null : $retr;
+		
+		}
+		
+		
+		public function Get ($key, $default=null) {
+		
+			return is_null($retr=$this->get_impl($key)) ? $default : $retr;
+		
+		}
+		
+		
+		public function __isset ($key) {
+		
+			return !is_null($this->get_impl($key));
+		
+		}
+		
+		
+		public function __get ($key) {
+		
+			$retr=$this->get_impl($key);
+			
+			if (is_null($retr)) throw new \Exception(
+				sprintf(
+					'"%s" does not exist',
+					$key
+				)
+			);
+			
+			return $retr;
+		
+		}
+		
+		
+		public function getIterator () {
+		
+			return $this->kv->getIterator();
+		
+		}
+	
+	
+	}
+
+
+?>
