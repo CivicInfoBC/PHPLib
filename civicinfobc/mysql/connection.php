c83a2bdf13dc73771b276bb66ffe48f83b882b31
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2014-07-30 13:48:58 -0700

    MySQL Escaping Change
    Booleans are no longer quoted when escaped.  True is transformed into 1, false into 0.

--- civicinfobc/mysql/connection.php
+++ civicinfobc/mysql/connection.php
@@ -116,8 +116,9 @@
 				($value instanceof Literal)
 			) return (string)$value;
 			
-			if (is_bool($value)) $value=$value ? 1 : 0;
-			else if ($value instanceof \DateTime) $value=$value->format('Y-m-d H:i:s');
+			if (is_bool($value)) return $value ? '1' : '0';
+			
+			if ($value instanceof \DateTime) $value=$value->format('Y-m-d H:i:s');
 			
 			return '\''.$this->mysqli->real_escape_string($value).'\'';
 		
