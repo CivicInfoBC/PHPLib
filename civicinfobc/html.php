23c965f26eb1264e228ead16e0e3d91950501a9c
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2014-10-02 15:14:32 -0700

    HTML::Parse
    Added the Parse static method to the \CivicInfoBC\HTML class.
    
    This method uses libxml to parse HTML into a \DOMDocument object.  In so doing it avoids the pitfalls of using \DOMDocument::loadHTML directly:
    
    -	It ignores warnings (unless $strict=true), rather than spitting them to stdout (if display of errors/warnings is enabled, which it should be in a development environment, which makes consuming HTML with libxml incredibly noisy)
    -	It translates errors (and warnings, with $strict=true) to exceptions

--- civicinfobc/html.php
+++ civicinfobc/html.php
@@ -29,6 +29,39 @@
 			echo(self::escape_array(func_get_args()));
 		
 		}
+		
+		
+		public static function Parse ($html, $strict=false) {
+		
+			$retr=new \DOMDocument();
+		
+			$prev=libxml_use_internal_errors(true);
+			libxml_clear_errors();
+			
+			$retr->loadHTML($html);
+			
+			$error=null;
+			foreach (libxml_get_errors() as $err) {
+			
+				if (!$strict && (
+					($err->level===LIBXML_ERR_WARNING) ||
+					($err->level===LIBXML_ERR_ERROR)
+				)) continue;
+				
+				$error=$err;
+				
+				break;
+			
+			}
+			
+			libxml_clear_errors();
+			libxml_use_internal_errors($prev);
+			
+			if (!is_null($error)) throw new \Exception($error->message,$error->code);
+			
+			return $retr;
+		
+		}
 	
 	
 	}
